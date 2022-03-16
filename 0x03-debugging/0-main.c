15d42ced7db0da153ba846473a8c80067f238e71
odilon-cloud <oirambna@gmail.com>
2022-03-16 21:22:35 +0000

    commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+*main - tests functions that prints if integer is positive or negative
+*return: 0
+*/
+int main(void)
+{
+	int i;
+	i=0;
+	postive_or_negative(i);
+	return (0);
+}
