6b3c9578fe2adba3f67610e109cb8f4984c04801
odilon-cloud <oirambna@gmail.com>
2022-03-24 20:31:18 +0000

    commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,31 +1,42 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *print_number - print a number using _putchar.
- *@n: the number to be printed.
+ *print_buffer - Print a buffer 10 bytes per line.
+ *@b: Buffer address.
+ *@size: Number of characters to be printed.
  */
-
-void print_number(int n)
+void print_buffer(char *b, int size)
 {
-	unsigned int i = 1;
+	int j, i, l;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
-	if (n == 0)
-		_putchar('0');
+	if (size <= 0)
+		printf("\n");
 	else
 	{
-		while ((n / i) >= 10)
-			i *= 10;
-
-		while (i > 0)
+		for (j = 0; j < size; j += 10)
 		{
-			_putchar((n / i) + '0');
-			n %= i;
-			i /= 10;
+			printf("%.8x:", j);
+			for (i = j; i < j + 10; i++)
+			{
+				if (i % 2 == 0)
+					printf(" ");
+				if (i < size)
+					printf("%.2x", *(b + i));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
 		}
 	}
 }
