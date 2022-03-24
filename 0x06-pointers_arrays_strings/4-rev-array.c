b33c2a8d55869a4e5c73f41dfc1c406646b67eea
odilon-cloud <oirambna@gmail.com>
2022-03-24 10:48:58 +0000

    commit

--- 0x06-pointers_arrays_strings/4-rev-array.c
+++ 0x06-pointers_arrays_strings/4-rev-array.c
@@ -1,40 +1,33 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * reverse_array - reverses the content of an array of integers
- * @a: an array of integers
- * @n: number of elements of the array
- * Return: returns void
- */
+ * reverse_array - a function that reverses the
+ *                content of an array of integers
+ *
+ * @a: pointer to int array
+ * @n: is the number of elements to swap
+ *
+ * Return: nothing
+*/
 
 void reverse_array(int *a, int n)
-
 {
-	int *start_c, *end_c, c;
-	int i;
+	int temp, s, e;
 
-	/*Start_c and end_c is equal value of variable s*/
-	start_c = a;
-	end_c = a;
-
-	/*Move the end_c to the last character*/
-	for (i = 0; i < n - 1; i++)
+	s = 0;
+	e = n - 1;
+	/**
+	 * set value in array a in temp
+	 * then place the last array in
+	 * the first array then place
+	 * value in temp to last array
+	*/
+	while (s < e)
 	{
-		end_c++;
+		temp = a[s];
+		a[s] = a[e];
+		a[e] = temp;
+		s++;
+		e--;
 	}
-
-	/* Swap the char from start and end */
-	/* index using begin_ptr and end_ptr */
-	for (i = 0; i < n / 2; i++)
-	{
-
-		/*swap character*/
-		c = *end_c;
-		*end_c = *start_c;
-		*start_c = c;
-
-		/*update pointers positions*/
-		start_c++;
-		end_c--;
-	}
-
 }
