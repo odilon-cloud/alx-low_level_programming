75b9e5bbf5137124875b121be34a33e17f4e9442
odilon-cloud <oirambna@gmail.com>
2022-04-06 18:26:32 +0000

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,13 +7,13 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *a;
 
-	p = malloc(b);
-	if (p == NULL)
+	a = malloc(b);
+	if (a == NULL)
 	{
 		exit(98);
 	}
 
-	return (p);
+	return (a);
 }
