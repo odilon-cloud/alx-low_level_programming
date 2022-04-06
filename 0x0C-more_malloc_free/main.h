dc965b95e3fdd379749634826f48d6f2552c0e96
odilon-cloud <oirambna@gmail.com>
2022-04-06 22:36:03 +0000

    fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,7 +12,7 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 /**
-*check_number -checks if string has one number
+*check_number - checks if string has one number
 *
 *@str: string
 *
