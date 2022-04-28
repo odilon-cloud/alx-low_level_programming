4e91b3d30c7cfd7a895d613365bb413911b10f32
odilon-cloud <oirambna@gmail.com>
2022-04-28 20:17:34 +0000

    commit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
+#include "holberton.h"
 /**
- * set_bit - sets the value of a bit to 1 at a given index
+ * clear_bit -  sets the value of a bit to 0 at a given index.
  * @n: number to check the value of
  * @index: the index to look for the number
  * Return: 1 if it worked, or -1 if an error occurred
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int number = *n;
